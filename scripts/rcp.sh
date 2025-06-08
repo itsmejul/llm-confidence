6e28363e49f37fe6feab89335d32bdd32e3bd622
Julian Mosig von Aehrenfeld <julianvonmosig@gmail.com>
2025-06-08 11:39:30 +0200

    run deepseek experiment

--- scripts/rcp.sh
+++ scripts/rcp.sh
@@ -30,6 +30,6 @@ else
 fi
 
 
-python /home/sc.uni-leipzig.de/gr15iped/dev/math-ml/correlation_pipeline.py --dataset_name=xsum --model_name="deepseek-ai/DeepSeek-R1"
+python /home/sc.uni-leipzig.de/gr15iped/dev/math-ml/correlation_pipeline.py --dataset_name=xsum --model_name="deepseek-ai/deepseek-llm-7b-base"
 
 
