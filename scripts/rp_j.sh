9b851360a14bcad6ba1b368cc20db77d177e6253
Julian Mosig von Aehrenfeld <julianvonmosig@gmail.com>
2025-06-27 15:47:35 +0200

    test exp

---
+++ scripts/rp_j.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=cod_rerun_llama2
+#SBATCH --partition=clara
+#SBATCH --gpus=v100
+#SBATCH --ntasks=1
+#SBATCH --mem=32G
+#SBATCH --time=24:00:00
+#SBATCH -o /home/sc.uni-leipzig.de/gr15iped/jobfiles/log/%x.out-%j
+#SBATCH -e /home/sc.uni-leipzig.de/gr15iped/jobfiles/log/%x.err-%j
+
+VENV_DIR="$HOME/dev/math-ml/.venv"
+REQ_FILE="$HOME/dev/math-ml/requirements.txt"
+
+
+module load Python/3.12
+
+source "$VENV_DIR/bin/activate"
+
+if [ -f "$REQ_FILE" ]; then
+	echo "Installin requirements..."
+	pip install --upgrade pip
+	pip install -r "$REQ_FILE"
+else
+	echo "Warning: Requirements.txt not found"
+fi
+
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True #reduce memory reserved for pytorch but unallocated
+python /home/sc.uni-leipzig.de/gr15iped/dev/math-ml/pipeline.py --experiment_name="llama2_cod_all" --n_samples=-1 --start_index=0 --model_name="meta-llama/Llama-3.1-8B" --device="cuda" --tokens_per_response=1000 --prompting_technique="cod" --rerun_buggy_samples="yes"
